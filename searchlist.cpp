e0c2fd54a8176c270026ef5143b4a6245c45ce9e
YRDF <zzh5653@163.com>
2024-10-30 14:31:13 +0800

    实现好友申请界面

--- searchlist.cpp
+++ searchlist.cpp
@@ -2,7 +2,7 @@
 #include<QScrollBar>
 #include "adduseritem.h"
 //#include "invaliditem.h"
-//#include "findsuccessdlg.h"
+#include "findsuccessdlg.h"
 #include "tcpmgr.h"
 #include "customizeedit.h"
 //#include "findfaildlg.h"
@@ -26,7 +26,10 @@ SearchList::SearchList(QWidget *parent):QListWidget(parent),_find_dlg(nullptr), 
 
 void SearchList::CloseFindDlg()
 {
-
+    if(_find_dlg){
+        _find_dlg->hide();
+        _find_dlg = nullptr;
+    }
 }
 
 void SearchList::SetSearchEdit(QWidget *edit)
@@ -42,26 +45,69 @@ void SearchList::waitPending(bool pending)
 void SearchList::addTipItem()
 {
     auto *invalid_item = new QWidget();
-        QListWidgetItem *item_tmp = new QListWidgetItem;
-        //qDebug()<<"chat_user_wid sizeHint is " << chat_user_wid->sizeHint();
-        item_tmp->setSizeHint(QSize(250,10));
-        this->addItem(item_tmp);
-        invalid_item->setObjectName("invalid_item");
-        this->setItemWidget(item_tmp, invalid_item);
-        item_tmp->setFlags(item_tmp->flags() & ~Qt::ItemIsSelectable);
+    QListWidgetItem *item_tmp = new QListWidgetItem;
+    //qDebug()<<"chat_user_wid sizeHint is " << chat_user_wid->sizeHint();
+    item_tmp->setSizeHint(QSize(250,10));
+    this->addItem(item_tmp);
+    invalid_item->setObjectName("invalid_item");
+    this->setItemWidget(item_tmp, invalid_item);
+    item_tmp->setFlags(item_tmp->flags() & ~Qt::ItemIsSelectable);
 
 
-        auto *add_user_item = new AddUserItem();
-        QListWidgetItem *item = new QListWidgetItem;
-        //qDebug()<<"chat_user_wid sizeHint is " << chat_user_wid->sizeHint();
-        item->setSizeHint(add_user_item->sizeHint());
-        this->addItem(item);
-        this->setItemWidget(item, add_user_item);
+    auto *add_user_item = new AddUserItem();
+    QListWidgetItem *item = new QListWidgetItem;
+    //qDebug()<<"chat_user_wid sizeHint is " << chat_user_wid->sizeHint();
+    item->setSizeHint(add_user_item->sizeHint());
+    this->addItem(item);
+    this->setItemWidget(item, add_user_item);
 }
 
 void SearchList::slot_item_clicked(QListWidgetItem *item)
 {
+    QWidget *widget = this->itemWidget(item); // 获取自定义widget对象
+    if(!widget){
+        qDebug()<< "slot item clicked widget is nullptr";
+        return;
+    }
 
+    // 对自定义widget进行操作， 将item 转化为基类ListItemBase
+    ListItemBase *customItem = qobject_cast<ListItemBase*>(widget);
+    if(!customItem){
+        qDebug()<< "slot item clicked widget is nullptr";
+        return;
+    }
+    auto itemType = customItem->GetItemType();
+    if(itemType == ListItemType::INVALID_ITEM){
+        qDebug()<< "slot invalid item clicked ";
+        return;
+    }
+    if(itemType == ListItemType::ADD_USER_TIP_ITEM){
+
+//        if (_send_pending) {
+//            return;
+//        }
+//        waitPending(true);
+//        auto search_edit = dynamic_cast<CustomizeEdit*>(_search_edit);
+//        auto uid_str = search_edit->text();
+//        //此处发送请求给server
+//        QJsonObject jsonObj;
+//        jsonObj["uid"] = uid_str;
+
+//        QJsonDocument doc(jsonObj);
+//        QString jsonString = doc.toJson(QJsonDocument::Indented);
+
+//        //发送tcp请求给chat server
+//        emit TcpMgr::GetInstance()->sig_send_data(ReqId::ID_SEARCH_USER_REQ, jsonString);
+        _find_dlg = std::make_shared<FindSuccessDlg>(this);
+        auto si = std::make_shared<SearchInfo>(0,"zzh1","zzh1","hello ,my friend!",0);
+        std::dynamic_pointer_cast<FindSuccessDlg>(_find_dlg)->SetSearchInfo(si);
+        qDebug()<<"slot ADD_USER_TIP_ITEM clicked ";
+        _find_dlg->show();
+        return;
+    }
+
+    //清除弹出框
+     CloseFindDlg();
 }
 
 void SearchList::slot_user_search(std::shared_ptr<SearchInfo> si)
